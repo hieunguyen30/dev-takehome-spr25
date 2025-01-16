f29c29220bda5d66dcafef151fca32eaa58e4636
tommynguyen <hieu.ntrung30@gmail.com>
2025-01-15 22:02:05 -0500

    front end and back end updated

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import Table from "@/components/tables/Table";
 import Button from "@/components/atoms/Button";
 import Input from "@/components/atoms/Input";
 import { useState } from "react";
@@ -19,31 +19,35 @@ export default function ItemRequestsPage() {
   };
 
   return (
-    <div className="max-w-md mx-auto mt-8 flex flex-col items-center gap-6">
-      <h2 className="font-bold">Approve Items</h2>
+    <div><Table /></div>
+    // <div className="max-w-md mx-auto mt-8 flex flex-col items-center gap-6">
+    //   <h2 className="font-bold">Approve Items</h2>
 
-      <div className="flex flex-col w-full gap-4">
-        <Input
-          type="text"
-          placeholder="Type an item"
-          value={item}
-          onChange={(e) => setItem(e.target.value)}
-          label="Item"
-        />
-        <Button onClick={handleAddItem}>Approve</Button>
-      </div>
-      <div className="flex flex-col gap-3">
-        <h3 className="underline">Currently approved items:</h3>
-        {itemList.length > 0 ? (
-          <ul className="list-disc pl-5">
-            {itemList.map((listItem, index) => (
-              <li key={index}>{listItem}</li>
-            ))}
-          </ul>
-        ) : (
-          "None :("
-        )}
-      </div>
-    </div>
+    //   <div className="flex flex-col w-full gap-4">
+    //     <Input
+    //       type="text"
+    //       placeholder="Type an item"
+    //       value={item}
+    //       onChange={(e) => setItem(e.target.value)}
+    //       label="Item"
+    //     />
+    //     <Button onClick={handleAddItem}>Approve</Button>
+    //   </div>
+    //   <div className="flex flex-col gap-3">
+    //     <h3 className="underline">Currently approved items:</h3>
+    //     {itemList.length > 0 ? (
+    //       <ul className="list-disc pl-5">
+    //         {itemList.map((listItem, index) => (
+    //           <li key={index}>{listItem}</li>
+    //         ))}
+    //       </ul>
+    //     ) : (
+    //       "None :("
+    //     )}
+    //   </div>
+    //   <div>
+    //     <Table></Table>
+    //   </div>
+    // </div>
   );
 }
--- src/app/cool/page.tsx
+++ src/app/cool/page.tsx
@@ -1,13 +1,8 @@
 export default function Kewl() {
   return (
     <div className="min-h-screen flex flex-col justify-center items-center bg-primary text-white gap-5">
-      {/* 
-      Write something unique about you here! 
-      It could be a club you're part of, a weird skill you have, or something special that happened to you.
-      Feel free to put links, images, whatever! 
-      Don't worry about styling- we aren't grading you on this- it's just to get to know you better! :) 
-      */}
-      😎😎😎
+      Hi! My name is Tommy and I'm a first year Computer Engineering major.<br/>
+      A fun fact about me is that I'm learning how to cook so that I don't have to eat Nav food next year.
     </div>
   );
 }
